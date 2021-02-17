d3e021c6e4bcb41b79743f6dbb137b2d927cf3a8
while95 <lpott95@gmail.com>
2021-02-17 23:55:23 +0900

    the last

--- src/store.js
+++ src/store.js
@@ -1,45 +1,12 @@
-import {createStore} from 'redux';
-import {createAction, createReducer, configureStore, createSlice} from '@reduxjs/toolkit';
-
-
-
-/*
-//without redux toolkit
-const reducer = (state = [], action) => {
-    
-    switch (action.type){
-       
-        case addToDo.type :
-            console.log(action)
-            return [{ text: action.payload, id: Date.now() },...state];
-        case deleteToDo.type:
-            return state.filter(toDo => toDo.id !== action.payload);
-        default :
-        return state;
-    }
-}
-*/
-
-/*
-//with redux toolkit
-const addToDo = createAction("ADD");
-const deleteToDo =  createAction("DELETE");
-
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
-  },
-  [deleteToDo]: (state, action) =>
-    state.filter(toDo => toDo.id !== action.payload)
-});
-*/
+import {configureStore, createSlice} from '@reduxjs/toolkit';
 
 //with redux toolkit createSlice
+
 
 const toDos = createSlice({
     name: 'toDosReducer',
     initialState: [],
-    reduecers: {
+    reducers: {
         add: (state, action) => {
             state.push({ text: action.payload, id: Date.now() })
         },
